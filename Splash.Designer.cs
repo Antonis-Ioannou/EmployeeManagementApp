e68e4dd30db874714068f504294ba98a05ea8c95
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-04-07 13:03:22 +0300

    splash load

--- Splash.Designer.cs
+++ Splash.Designer.cs
@@ -29,9 +29,15 @@ namespace EmployeeManagementApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.splashTitle = new System.Windows.Forms.Label();
             this.splashCredit = new System.Windows.Forms.Label();
             this.MyProgress = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MyProgress.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // splashTitle
@@ -39,7 +45,7 @@ namespace EmployeeManagementApp
             this.splashTitle.AutoSize = true;
             this.splashTitle.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.splashTitle.ForeColor = System.Drawing.Color.DarkCyan;
-            this.splashTitle.Location = new System.Drawing.Point(107, 9);
+            this.splashTitle.Location = new System.Drawing.Point(12, 9);
             this.splashTitle.Name = "splashTitle";
             this.splashTitle.Size = new System.Drawing.Size(316, 28);
             this.splashTitle.TabIndex = 0;
@@ -50,7 +56,7 @@ namespace EmployeeManagementApp
             this.splashCredit.AutoSize = true;
             this.splashCredit.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.splashCredit.ForeColor = System.Drawing.Color.DarkCyan;
-            this.splashCredit.Location = new System.Drawing.Point(180, 225);
+            this.splashCredit.Location = new System.Drawing.Point(84, 379);
             this.splashCredit.Name = "splashCredit";
             this.splashCredit.Size = new System.Drawing.Size(175, 17);
             this.splashCredit.TabIndex = 1;
@@ -58,25 +64,41 @@ namespace EmployeeManagementApp
             // 
             // MyProgress
             // 
+            this.MyProgress.Controls.Add(this.pictureBox1);
             this.MyProgress.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
             this.MyProgress.FillThickness = 10;
             this.MyProgress.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.MyProgress.ForeColor = System.Drawing.Color.White;
-            this.MyProgress.Location = new System.Drawing.Point(195, 55);
+            this.MyProgress.Location = new System.Drawing.Point(28, 53);
             this.MyProgress.Minimum = 0;
             this.MyProgress.Name = "MyProgress";
+            this.MyProgress.ProgressColor = System.Drawing.Color.DarkCyan;
+            this.MyProgress.ProgressColor2 = System.Drawing.Color.LightCyan;
             this.MyProgress.ProgressThickness = 10;
             this.MyProgress.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.MyProgress.Size = new System.Drawing.Size(160, 160);
+            this.MyProgress.Size = new System.Drawing.Size(288, 288);
             this.MyProgress.TabIndex = 2;
             this.MyProgress.Text = "guna2CircleProgressBar1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 177);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(523, 251);
+            this.ClientSize = new System.Drawing.Size(342, 405);
             this.Controls.Add(this.MyProgress);
             this.Controls.Add(this.splashCredit);
             this.Controls.Add(this.splashTitle);
@@ -84,6 +106,9 @@ namespace EmployeeManagementApp
             this.Name = "Splash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Splash_Load);
+            this.MyProgress.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +119,8 @@ namespace EmployeeManagementApp
         private System.Windows.Forms.Label splashTitle;
         private System.Windows.Forms.Label splashCredit;
         private Guna.UI2.WinForms.Guna2CircleProgressBar MyProgress;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Splash.cs
+++ Splash.cs
@@ -12,9 +12,30 @@ namespace EmployeeManagementApp
 {
     public partial class Splash : Form
     {
+        int startPoint = 0;
+
         public Splash()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startPoint += 1;
+            MyProgress.Value = startPoint;
+            if (MyProgress.Value==100)
+            {
+                MyProgress.Value = 0;
+                timer1.Stop();
+                this.Hide();
+                Login login = new Login();
+                login.Show();
+            }
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
